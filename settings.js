4c53dde493c18037af097753445f2b2c84903399
Darlington5555 <154468318+Darlington5555@users.noreply.github.com>
2025-01-07 21:03:59 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "MTN MOMO"
 
-global.banknumber = "8133729715"
+global.momonum = "0599154415"
 
-global.bankowner = "FADARE"
+global.bankowner = "DUTY"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
